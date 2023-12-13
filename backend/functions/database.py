ef06e92f582556992ddd8f367221fc4c8dc136a3
Anmitchell <avarynmitchell@gmail.com>
2023-12-12 20:41:23 -0500

    Setting up local database

---
+++ backend/functions/database.py
@@ -0,0 +1,45 @@
+import json
+import random
+
+# Get recent messages
+def get_recent_messages():
+
+    # Define the file name and learn instructions
+    file_name = "stored_data.json"
+    learn_instruction = {
+        "role": "system",
+        # instructions to frame thought process of AI --> Prompt engineering
+        "content": "You are interviewing the user for a job as a retail assistant. Ask short questions that are relevant to the junior position. Your name is Rachel the user is called Avery. Keep your answers to under 30 words"
+    }
+
+    # Initialize messages
+    messages = []
+
+    # Add a random element
+    x = random.unform(0, 1)
+    if x < 0.5:
+        learn_instruction["content"] = learn_instruction["content"] + " Your response will include some dry humour."
+    else:
+         learn_instruction["content"] = learn_instruction["content"] + " Your response will include a rather challenging question."
+
+    # Append instruction to message
+    messages.append(learn_instruction)
+
+    # Get last messages
+    try:
+        with open(file_name) as user_file:
+            data = json.load(user_file)
+
+            # Append last 5 items of data
+            if data:
+                if len(data) < 5:
+                    for item in data:
+                        messages.append(item)
+                    else:
+                        for item in data[-5]:
+                            messages.append(item)
+    except Exception as e:
+        print(e)
+        pass
+
+    return messages
