44f9107b77aa1f4f04e41db4536cf52b7b93c703
Anmitchell <avarynmitchell@gmail.com>
2023-12-24 10:20:06 -0500

    updated bash script

--- run_prod_server.sh
+++ run_prod_server.sh
@@ -6,4 +6,4 @@ cd ..
 cd backend
 source venv/bin/activate
 pip install -r installedPackages.txt
-gunicorn -w 1 -k uvicorn.workers.UvicornWorker main:app -b 0.0.0.0:8000
+# gunicorn -w 1 -k uvicorn.workers.UvicornWorker main:app -b 0.0.0.0:8000
