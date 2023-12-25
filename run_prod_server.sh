7c35ad4344ce7460884a3ace3fe99f2d31cf8600
Anmitchell <avarynmitchell@gmail.com>
2023-12-24 23:20:04 -0500

    updated bash script

--- run_prod_server.sh
+++ run_prod_server.sh
@@ -5,5 +5,5 @@ npm run build
 cd ../backend/
 python3.11 -m venv .venv # create virtual environement for python app
 source .venv/bin/activate # activate virtual environment
-sudo pip install -r installedPackages.txtdd # install all python packages
+sudo pip install -r installedPackages.txt # install all python packages
 # gunicorn -w 1 -k uvicorn.workers.UvicornWorker main:app -b 0.0.0.0:8000
