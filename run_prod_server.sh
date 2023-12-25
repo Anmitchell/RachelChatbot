6bee2f370a2b9a82e3b6c6c21f7faf73ebc775ab
Anmitchell <avarynmitchell@gmail.com>
2023-12-24 20:31:16 -0500

    update bash script file

--- run_prod_server.sh
+++ run_prod_server.sh
@@ -4,6 +4,7 @@ npm i
 npm run build
 cd ..
 cd backend
-source venv/bin/activate
+python3.11 -m venv .venv
+source .venv/bin/activate
 pip install -r installedPackages.txt
 # gunicorn -w 1 -k uvicorn.workers.UvicornWorker main:app -b 0.0.0.0:8000
